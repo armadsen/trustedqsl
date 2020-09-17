848ea07f905551467ba0591ee88e4f31d2874a37
Rick Murphy <k1mu@arrl.net>
2020-09-17 12:51:31 -0400

    Update translation sources, add Swedish translation

--- apps/lang/makemo.sh
+++ apps/lang/makemo.sh
@@ -1,4 +1,4 @@
-for lang in de es fi fr hi_IN it ja pl_PL pt ru tr_TR zh
+for lang in de es fi fr hi_IN it ja pl_PL pt ru sv_SE tr_TR zh
 do
 	msgfmt $lang/tqslapp.po -o $lang/tqslapp.mo
 done
--- apps/lang/merge.sh
+++ apps/lang/merge.sh
@@ -1,4 +1,4 @@
-for lang in de es fi fr hi_IN it ja pl_PL pt ru tr_TR zh
+for lang in de es fi fr hi_IN it ja pl_PL pt ru sv_SE tr_TR zh
 do
 	msgmerge -N -U $lang/tqslapp.po ../tqslapp.pot
 done
